4faea61c6dc69009a1a0838eeb41b9af1684cf3e
Ana Laura <analaurodrigues.ramos@gmail.com>
2025-04-30 16:14:31 -0300

    Add files via upload

--- CorridaMaluca.java
+++ CorridaMaluca.java
@@ -1,10 +1,51 @@
 package aula11;
 
+import java.util.Random;
+import java.util.Scanner;
+
 public class CorridaMaluca {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
+		Scanner ler = new Scanner(System.in);
+		Random random = new Random();
+		String[] corredores = new String[3];
+		int[] posicoes = {0, 0, 0};
 
+		System.out.println("Bem-vindos à Corrida Maluca!");
+
+		for (int i = 0; i < 3; i++) {
+		System.out.print("Digite o nome do corredor " + (i + 1) + ": ");
+		corredores[i] = ler.nextLine();
+		}
+
+		System.out.println("\nComeçou a corrida!");
+
+		for (int volta = 1; volta <= 5; volta++) {
+		System.out.println("\n--- Volta " + volta + " ---");
+		for (int i = 0; i < 3; i++) {
+		int avanco = random.nextInt(10) + 1;
+		posicoes[i] += avanco;
+		System.out.println(corredores[i] + " está na posição: " + posicoes[i] + " metros.");
+		}
+		}
+
+		System.out.println("\n - Fim da Corrida! -");
+
+		int campeaoIndice = 0;
+		for (int i = 1; i < 3; i++) {
+		if (posicoes[i] > posicoes[campeaoIndice]) {
+		campeaoIndice = i;
+		}
+		}
+
+		System.out.println("O campeão da Corrida Maluca é: " + corredores[campeaoIndice] + "!");
+
+		ler.close();
+
+		}
+
+		
 	}
 
-}
+
---
+++ NumerosMagicos.java
@@ -0,0 +1,10 @@
+package aula11;
+
+public class NumerosMagicos {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
