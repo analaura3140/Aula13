c49bbb9fc86318c02c6778934c431fd8c4c38908
Ana Laura <analaurodrigues.ramos@gmail.com>
2025-05-07 11:14:48 -0300

    Add files via upload

---
+++ Contagem.java
@@ -0,0 +1,17 @@
+package aula11;
+
+public class Contagem {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+int num = 100;
+
+while (num>= 0) {
+	System.out.println(num);
+	num = num - 10;
+}
+
+}
+	}
+
+
